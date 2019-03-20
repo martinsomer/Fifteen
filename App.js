018a8665b7e2e24d55caa9fefe4c32ff2c2b2df3
martinsomer <martinsom35@gmail.com>
2019-03-20 22:10:51 +0200

    Basic grid functionality

--- App.js
+++ App.js
@@ -1,27 +1,98 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Tile from './components/Tile';
+
+// Array for holding all tiles
+const tiles = [];
+
+// Object for tile
+const tile = {
+    index: null,
+    pos_x: null,
+    pos_y: null,
+    isEmpty: false,
+}
 
 type Props = {};
 export default class App extends Component<Props> {
+    
+    // Create tiles when component starts
+    constructor(props) {
+        super(props);
+        
+        let index = 0;
+        
+        for (let i=0; i<4; i++) {
+            const children = [];
+            
+            for (let j=0; j<4; j++) {
+                const t = Object.create(tile);
+                t.index = index;
+                t.pos_x = i;
+                t.pos_y = j;
+                i === 3 && j === 3 ? t.isEmpty = true : t.isEmpty = false; // Last tile is empty
+                children.push(t);
+                
+                index++;
+            }
+            tiles.push(children);
+        }
+    }
+    
+    // Create the table for rendering
+    createTable = () => {
+        let table = [];
+
+        // Outer loop to create parent
+        for (let i=0; i<4; i++) {
+            let children = [];
+            
+            // Inner loop to create children
+            for (let j=0; j<4; j++) {
+                
+                // Check if a tile exists at this table index
+                if (!tiles[i][j].isEmpty) {
+                    children.push(<Tile key={tiles[i][j].index} onPress={() => this.tileTapped(tiles[i][j].index)}></Tile>);
+                } else {
+                    children.push(<View style={styles.tableCell}></View>);
+                }
+            }
+            
+            // Create the parent and add the children
+            table.push(<View style={styles.tableRow}>{children}</View>);
+        }
+        return table;
+    }
+    
+    // Respond to touches
+    tileTapped(key) {
+        alert("Tile ID: " + key);
+    }
+    
+    // Main render function
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                {this.createTable()}
             </View>
         );
     }
 }
 
+// Stylesheet
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    tableRow: {
+        flexDirection: "row",
+    },
+    tableCell: {
+        flex: 1,
+        aspectRatio: 1,
+        backgroundColor: 'darkgray',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 });
---
+++ components/Tile.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+type Props = {};
+export default class Tile extends Component<Props> {
+    render() {
+        return (
+            <TouchableOpacity style={styles.tile} onPress={this.props.onPress}>
+                <Text style={styles.tileNumber}>?</Text>
+            </TouchableOpacity>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    tile: {
+        flex: 1,
+        aspectRatio: 1,
+        borderRadius: 5,
+        margin: 5,
+        marginRight: 2.5,
+        marginLeft: 2.5,
+        backgroundColor: '#00bfa5',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tileNumber: {
+        color: 'rgba(255, 255, 255, 0.8)',
+        fontSize: 50,
+    }
+});
